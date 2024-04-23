f208805b62557c222f08bb9b351f6712f82ba77e
camila-stefano <camilavictoriastefano@gmail.com>
2024-04-22 22:01:51 -0300

    updated

--- src/components/Li.jsx
+++ src/components/Li.jsx
@@ -1,6 +1,6 @@
 const Li = ({ children }) => {
     return (
-        <li className="cursor-pointer font-semibold text-primaryGreen tracking-wider">
+        <li className="cursor-pointer font-semibold text-primaryGreen tracking-wider hover:text-secondaryGreen transition ease-in-out delay-100 ">
             <a>{ children }</a>
         </li>
     )
