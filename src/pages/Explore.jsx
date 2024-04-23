d8174addf30fcea0a82d2ee8cfe09037c27f2831
camila-stefano <camilavictoriastefano@gmail.com>
2024-04-23 20:58:15 -0300

    add Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,24 @@
+import Layout from "../components/Layout";
+import usePlants from "../hooks/usePlants";
 
-import Layout from '../components/Layout'
 
 const Explore = () => {
-  return (
-    <Layout>Explore</Layout>
-  )
-}
+  const { data } = usePlants();
 
-export default Explore
+  return (
+    <Layout>
+      <div className="flex flex-col gap-4">
+        <h1>Explore</h1>
+        {data && (
+          <ul>
+            {data.map((plant) => (
+              <li key={plant.id}>{plant.common_name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default Explore;
