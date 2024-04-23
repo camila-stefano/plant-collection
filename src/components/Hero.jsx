0c9c9b164769794576e53db169d4f5035972ec89
camila-stefano <camilavictoriastefano@gmail.com>
2024-04-23 18:11:45 -0300

    update main and Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import baclgroundImage from "../assets/background.jpg";
 const Hero = () => {
   return (
     <section className="container mx-auto flex h-96 selection:bg-primaryPink selection:text-light">
-      <div className="flex flex-col justify-center gap-y-4 text-primaryGreen">
-        <h1 className="font-bold text-header">Find your new plant friend!</h1>
-        <p className="text-lg">
-          Lorem ipsum jaskajskjdhsdhahdkhadkhsdkhkasdhsgdfsfja
+      <div className="flex flex-1 flex-col justify-center gap-y-4 text-primaryGreen font-maison pr-16">
+        <h1 className="font-bold text-header tracking-wide">Explore the world of plants</h1>
+        <p className="text-lg">Discover the Beauty and Diversity of Plants: Explore our Website
         </p>
+        <button className="bg-primaryGreen text-primaryPink rounded-full w-fit py-2 px-6">Explore</button>
       </div>
-      <div className="basis-1/2">
-        <img className="h-[40rem] w-full object-cover" src={baclgroundImage} alt="Plant image" />
+      <div className="flex-none">
+        <img className="h-[50rem] w-full object-cover" src={baclgroundImage} alt="Plant image" />
       </div>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Explore from "./pages/Explore.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "explore", element: <Explore /> }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
