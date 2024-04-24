781c60d2034ea75d1661ea11d74df6658808aaa5
camila-stefano <camilavictoriastefano@gmail.com>
2024-04-24 00:07:38 -0300

    Delete Li component

--- src/components/Li.jsx
+++
@@ -1,9 +0,0 @@
-const Li = ({ children }) => {
-    return (
-        <li className="cursor-pointer font-semibold text-primaryGreen tracking-wider hover:text-secondaryGreen transition ease-in-out delay-100 ">
-            <a>{ children }</a>
-        </li>
-    )
-}
-
-export default Li
