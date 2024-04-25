9bff752123e0a3b606a21c464e233550d62a074c
camila-stefano <camilavictoriastefano@gmail.com>
2024-04-24 23:39:16 -0300

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="container mx-auto py-5 flex justify-between font-maison">
+        <div className="flex justify-center">
+            <p className="">2024</p><p className="">Made with love by Camila Stefano</p>
+        </div>
+        <div className="flex">
+            <a
+            href="https://github.com/deptagency-dar/dar-employee-handbook"
+            className="w-10 flex justify-center text-primaryPink hover:text-gray-500"
+            >
+            <span className="sr-only">GitHub</span>
+            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path
+                fillRule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clipRule="evenodd"
+                />
+            </svg>
+            </a>
+            <a
+            href="https://www.linkedin.com/company/deptagency/mycompany/"
+            className="w-10 flex justify-center text-primaryPink hover:text-gray-500"
+            >
+            <span className="sr-only">LinkedIn</span>
+            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 512 512">
+                <path d="M444.17 32H70.28C49.85 32 32 46.7 32 66.89v374.72C32 461.91 49.85 480 70.28 480h373.78c20.54 0 35.94-18.21 35.94-38.39V66.89C480.12 46.7 464.6 32 444.17 32zm-273.3 373.43h-64.18V205.88h64.18zM141 175.54h-.46c-20.54 0-33.84-15.29-33.84-34.43 0-19.49 13.65-34.42 34.65-34.42s33.85 14.82 34.31 34.42c-.01 19.14-13.31 34.43-34.66 34.43zm264.43 229.89h-64.18V296.32c0-26.14-9.34-44-32.56-44-17.74 0-28.24 12-32.91 23.69-1.75 4.2-2.22 9.92-2.22 15.76v113.66h-64.18V205.88h64.18v27.77c9.34-13.3 23.93-32.44 57.88-32.44 42.13 0 74 27.77 74 87.64z"></path>
+            </svg>
+            </a>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
